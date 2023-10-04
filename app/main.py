8058d9535917dffffb954f4102d655db89c51d51
Shoky13 <alla-alla-luk@yandex.ru>
2023-10-04 20:41:55 +0300

    initial commit

---
+++ app/main.py
@@ -0,0 +1,53 @@
+import fastapi as _fastapi
+import sqlalchemy.orm as _orm
+from fastapi.middleware.cors import CORSMiddleware
+
+import app.services as _services, app.schemas as _schemas, app.model as _model
+
+app = _fastapi.FastAPI()
+
+
+@app.post("/api/words")
+async def create_word(
+        word: _schemas.WordCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)
+):
+    db_word = await _services.get_word_by_simplified(word.simplified, db)
+    if db_word:
+        raise _fastapi.HTTPException(status_code=400, detail="Word is already in use")
+
+    return await _services.create_word(word, db)
+
+
+@app.get('/')
+def hello_world():
+    return {'hello': 'world'}
+
+
+@app.get("/api/words")
+def get_words(db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    return db.query(_model.Word).all()
+
+
+origins = [
+    "https://lukyanenkoa.github.io/web_ver_rus_zh_translator/",
+    "http://localhost:8000",
+    "http://127.0.0.1:8000/api/words/",
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/words/{word}", response_model=_schemas.Word)
+async def get_word(word, db: _orm.Session = _fastapi.Depends(_services.get_db)):
+    # get translated word by simplified version
+    db_word = db.query(_model.Word).filter(_model.Word.simplified == word).first()
+    if db_word is None:
+        return _fastapi.responses.JSONResponse(status_code=404, content={"message": "No such word found"})
+    return db_word
---
+++ app/services.py
@@ -0,0 +1,36 @@
+import sqlalchemy.orm as _orm
+import app.database as _database, app.model as _model, app.schemas as _schemas
+
+
+def create_database():
+    return _database.Base.metadata.create_all(bind=_database.engine)
+
+
+def get_db():
+    db = _database.SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+async def get_word_by_simplified(simplified: str, db: _orm.Session):
+    return db.query(_model.Word).filter(_model.Word.simplified == simplified).first()
+
+
+async def create_word(word: _schemas.WordCreate, db: _orm.Session):
+    word_obj = _model.Word(
+        traditional=word.traditional, simplified=word.simplified, english=word.english,
+        pinyin=word.pinyin, hsk=word.hsk)
+    db.add(word_obj)
+    db.commit()
+    db.refresh(word_obj)
+    return word_obj
+
+# async def fill_in(list_of_dicts: List, db: _orm.Session):
+#     for one_dict in list_of_dicts:
+#         new_word = _model.Word(traditional=one_dict['traditional'], simplified=one_dict['simplified'],
+#                                english=one_dict['english'], pinyin=one_dict['pinyin'], hsk=1)
+#         db.add(new_word)
+#         db.commit()
+#         db.refresh(new_word)
