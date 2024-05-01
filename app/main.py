3dab537b8b6bc352b796b4ac8d15910fb49d6240
Shoky13 <alla-alla-luk@yandex.ru>
2024-05-01 11:48:08 +0300

    fixed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 import fastapi as _fastapi
 import sqlalchemy.orm as _orm
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import Response
+from fastapi.requests import Request
+
+import requests
+import re
+from pydantic import BaseModel
 
 import app.services as _services, app.schemas as _schemas, app.model as _model
 
@@ -9,7 +15,7 @@ app = _fastapi.FastAPI()
 
 @app.post("/words")
 async def create_word(
-        word: _schemas.WordCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)
+    word: _schemas.WordCreate, db: _orm.Session = _fastapi.Depends(_services.get_db)
 ):
     db_word = await _services.get_word_by_simplified(word.simplified, db)
     if db_word:
@@ -20,7 +26,7 @@ async def create_word(
 
 @app.post("/words_rus")
 async def create_word(
-        word: _schemas.WordCreateRUS, db: _orm.Session = _fastapi.Depends(_services.get_db)
+    word: _schemas.WordCreateRUS, db: _orm.Session = _fastapi.Depends(_services.get_db)
 ):
     db_word = await _services.get_word_by_simplified_rus(word.simplified, db)
     if db_word:
@@ -28,9 +34,10 @@ async def create_word(
 
     return await _services.create_word_rus(word, db)
 
-@app.get('/')
+
+@app.get("/")
 def hello_world():
-    return {'hello': 'world'}
+    return {"hello": "world"}
 
 
 @app.get("/words")
@@ -47,7 +54,7 @@ origins = [
     "https://lukyanenkoa.github.io/web_ver_rus_zh_translator/",
     "http://localhost:8000",
     "http://127.0.0.1:8000/api/words/",
-    "*"
+    "*",
 ]
 
 app.add_middleware(
@@ -64,7 +71,9 @@ async def get_word(word, db: _orm.Session = _fastapi.Depends(_services.get_db)):
     # get translated word by simplified version
     db_word = db.query(_model.Word).filter(_model.Word.simplified == word).first()
     if db_word is None:
-        return _fastapi.responses.JSONResponse(status_code=404, content={"message": "No such word found"})
+        return _fastapi.responses.JSONResponse(
+            status_code=404, content={"message": "No such word found"}
+        )
     return db_word
 
 
@@ -73,6 +82,34 @@ async def get_word(word, db: _orm.Session = _fastapi.Depends(_services.get_db)):
     # get translated word by simplified version
     db_word = db.query(_model.WordRUS).filter(_model.WordRUS.simplified == word).first()
     if db_word is None:
-        return _fastapi.responses.JSONResponse(status_code=404, content={"message": "No such word found"})
+        return _fastapi.responses.JSONResponse(
+            status_code=404, content={"message": "No such word found"}
+        )
     return db_word
 
+
+@app.get("/stroke-order")
+async def stroke_order(q: str):
+    response = requests.get("http://www.strokeorder.info/mandarin.php?q=" + q)
+    imgsrc = re.findall(
+        'src="(http://bishun.strokeorder.info/characters/.+gif)"',
+        response.content.decode(),
+    )[0]
+
+    response = requests.get(imgsrc)
+    headers = response.headers
+    content = response.content
+    return Response(
+        content=content, headers={"Content-Type": headers.get("Content-Type")}
+    )
+
+
+class GoqhanziResponse(BaseModel):
+    results: list[str]
+
+
+@app.post("/goqhanzi")
+async def goqhanzi(request: Request):
+    body = await request.body()
+    response = requests.post("https://www.qhanzi.com/goqhanzi/", body.decode())
+    return GoqhanziResponse.model_validate_json(response.content)
